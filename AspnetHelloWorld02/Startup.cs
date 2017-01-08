7ae23a6381131e264c0eb5490d5a2c476ea96f25
root <hongxi@cloudtogo.cn>
2017-01-08 18:37:57 +0800

    Add project files.

---
+++ AspnetHelloWorld02/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspnetHelloWorld02.Startup))]
+namespace AspnetHelloWorld02
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
